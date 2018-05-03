4076012e3e5510284448432ec4c574ea51d7f8b4
Shereen Elaidi <shereen.elaidi@mail.mcgill.ca>
2018-05-03 11:43:38 -0400

    Version 1 of delta function

--- exocartographer/delta_function.py
+++ exocartographer/delta_function.py
@@ -1,80 +1,127 @@
-#STILL WORK IN PROGRESS
+import numpy as np 
 
-#importing modules 
+def analytic_delta(lat, long, w_rot, w_orb, obliq, i, sol_phase, orb_pos, sub_long, time, theta, phi): 
 
-import numpy as np
+    #First checking to make sure all input parameters are in bounds 
+
+    #w_orb
+    if(w_orb < 0):
+        raise ValueError("Values for w_orb must be bigger than 0.")
+
+    #Obliquity 
+    elif (obliq > np.pi/2 or obliq < 0 ): 
+        raise ValueError("Values for obliquity must be [0, pi/2]")
+
+    #Inclination 
+    elif (i < 0 or i > np.pi): 
+        raise ValueError("Values for inclination must be [0,pi]")
     
-
-def analytic_delta(lat, long, w_rot, w_orb, obliq, i, solstice_phase, time):
-
-    #First: checking to ensure that all parameters are in the proper values 
-
-
-    #Ranges
-
-    #w_orb 
-    if (not(w_orb > 0)): 
-        raise ValueError("Orbital angular frequency must be in ]0,+infinity[.")
-
-    #obliq 
-    elif (not((0 <= obliq) and ((np.pi*(1/2))>= obliq))): 
-        raise ValueError("Obliquity angular frequency must be in [0,pi/2]")
-    #i
-    elif (not((i >= 0) and (i <= np.pi))): 
-        raise ValueError("Orbital inclination must be between [0, pi]")
-
-    #solstice phase 
-    elif(not((solstice_phase >= 0) and (solstice_phase < (2*np.pi)))): 
-        raise ValueError("Solstice phase must be in [0,2pi[")
+    #Solstice phase 
+    elif (sol_phase < 0 or sol_phase >= 2*np.pi): 
+        raise ValueError("Values for solstice phase must be [0,2pi[")
     else: 
-        print("All is well")
-
-    #Setting each of the starting orbital positions for each value of t in the array 
+        print("All is well.")
+    
     flux = []
-    for i in time: 
-        t = time[i]
-        
-        #print("Loop iteration ", t)
-        starting_orbital_position = -1*w_rot*t
-        sub_observer_longitude =  w_orb*t
 
-        cos_theta_knot = np.cos(i)*np.cos(obliq) + np.sin(i)*np.sin(obliq)*np.cos(solstice_phase)
-        sin_theta_knot = np.sqrt(1-(cos_theta_knot)**2)
-        sin_theta_s = np.sqrt(1-np.sin(obliq)**2 * np.cos(w_orb*t - solstice_phase))
-        cos_theta_s = np.sqrt(np.sin(obliq)*np.cos(w_orb*t - solstice_phase))
+    for t in time: 
 
-        #Debugging
+        cos_phi_knot = np.cos(w_rot*t)
+        #print("Debugging: cos_phi_knot:")
+        #print(cos_phi_knot)
+
+        cos_theta_knot = np.cos(i)*np.cos(obliq) + np.sin(i)*np.sin(obliq)*np.cos(sol_phase)
+        #print("Debugging: cos_theta_knot:")
         #print(cos_theta_knot)
+
+        sin_theta_knot = np.sqrt(1-(cos_theta_knot)**2)
+        #print("Debugging: sin_theta_knot")
         #print(sin_theta_knot)
-        #print(sin_theta_s)
-        #print(cos_theta_s)
+
+        sin_theta = np.sin(theta)
+        #print("Debugging: sin_theta")
+        #print(sin_theta)
+
+        cos_theta = np.cos(theta)
+        #print("Debugging: cos_theta")
+        #print(cos_theta)
+
+        cos_theta_s = np.sin(obliq)*np.cos(w_orb*t - sub_long)
+        #print("Debugging: cos_theta_s")
+        #print(cos_theta_s) 
+
+        sin_phi_knot = -np.sin(w_rot*t)  
+        #print("Debugging: sin_phi_knot")
+        #print(sin_phi_knot) 
+
+        sin_theta_s = np.sqrt(1-(cos_theta_s)**2)          
+        #print("Debugging: sin_theta_s")
+        #print(sin_theta_s)  
 
         cos_piw = np.sin(i)*np.cos(w_orb*t)
-        cos_sknot = ((cos_piw)-(cos_theta_knot*cos_theta_s))/(sin_theta_knot*sin_theta_s)
+        #print("Debugging: cos_piw")
+        #print(cos_piw) 
 
-        sin_phi_s_phi_knot = (1/(sin_theta_knot*sin_theta_s))*(np.sqrt(1-(cos_theta_knot)**2 - (cos_theta_s)**2 - (cos_piw)**2 + 2*cos_theta_knot*cos_theta_s * cos_piw))
-        cos_phi_s = np.cos(w_rot*t)*cos_sknot - ((-1)*np.sin(w_rot*t)*sin_phi_s_phi_knot)
+        cos_phi_knot_s = (cos_piw - (cos_theta_knot*cos_theta_s))/(sin_theta_knot * sin_theta_s)
+        #print("Debugging: cos_phi_knot_s")
+        #print(cos_phi_knot_s) 
 
-        sin_phi_s = (((-np.sin(w_rot*t)*(np.sin(i)*np.cos(w_orb*t)- cos_theta_knot*np.sin(obliq)*np.cos(w_orb*t - solstice_phase)))
-            + np.cos(w_rot*t)*(np.sin(i)*np.sin(w_orb*t)*np.cos(obliq)-np.cos(i)*np.sin(obliq)*np.sin(w_orb*t-solstice_phase)))/
-            (np.sqrt(1-(np.cos(i)*np.cos(obliq) + np.sin(i)*np.cos(obliq)*np.cos(solstice_phase))**2) 
-            * np.sqrt(1-((np.sin(obliq)**2)*(np.cos(w_orb*t - solstice_phase))**2))))
+        sin_phi_knot_s = np.sqrt(1-(cos_phi_knot_s)**2)                           
+        #print("Debugging: sin_phi_knot_s:")
+        #print(sin_phi_knot_s) 
 
-        cos_phi_phiknot = np.cos(lat)*np.cos(w_rot*t) + np.sin(lat)*((-1)*np.sin(w_rot*t))
-        cos_phi_phis = np.cos(lat)*cos_phi_s + np.sin(lat)*sin_phi_s 
+        sin_phi_s = sin_phi_knot*cos_phi_knot_s + cos_phi_knot*sin_phi_knot_s
+        #print("Debugging sin_phi_s:")
+        #print(sin_phi_s)
 
-        #Illumination 
-        I = np.sin(long)*sin_theta_s*cos_phi_phis + np.cos(long)*cos_theta_s
-        #Visibility 
-        v = np.sin(long)*sin_theta_knot*cos_phi_phiknot + np.cos(long)*cos_theta_knot
+        cos_phi_s = cos_phi_knot*cos_phi_knot_s - sin_phi_knot*sin_phi_knot_s
+        #print("Debugging cos_phi_s")
+        #print(cos_phi_s)
 
-        flux.append((1/np.pi)*(max(v,0)*max((I,0)))) 
+        #Visibility and illumination 
 
-    return flux
+        cos_phi_phi_knot = np.cos(phi)*cos_phi_knot + np.sin(phi)*sin_phi_knot
+        #print("Debugging cos_phi_phi_knot")
+        #print(cos_phi_phi_knot)
+
+        cos_phi_phi_s = np.cos(phi)*cos_phi_s + np.sin(phi)*sin_phi_s 
+        #print("Debugging cos_phi_phi_s")
+        #print(cos_phi_phi_s)
+
+        Vnz = np.sin(theta)*sin_theta_knot*cos_phi_phi_knot + np.cos(theta)*cos_theta_knot 
+        
+        #print("Debugging Visbility nz: ")
+        #print(Vnz)
+
+        Inz = np.sin(theta)*sin_theta_s * cos_phi_phi_s + np.cos(theta)*cos_theta_s 
+        #print("Debugging Illumination nz")
+        #print(Inz)
+
+        single_flux = (1/np.pi)*(max(Vnz, 0))*(max(Inz, 0))
+        #print("Debugging single_flux")
+        #print(single_flux)
+
+        flux.append(single_flux)
+    
+    return flux 
+
+
 
 #Testing
-times = [1,2,3,4,5,5]
-print(analytic_delta(1,1,1,1,1,1,1,times))
+time = [1,1,1]
+lat = 2
+long = 1 
+w_rot = 1
+w_orb = 1
+obliq = 1
+i =1 
+sol_phase = 1
+orb_pos = 1
+sub_long = 1
+theta = np.pi/2
+phi = np.pi/2
+
+print(analytic_delta(lat, long, w_rot, w_orb, obliq, i, sol_phase, orb_pos, sub_long, time, theta, phi))
 
 
         
