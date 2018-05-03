ebb787d80639c82c8c5abc426f0b5e6d38fc93b9
Shereen Elaidi <shereen.elaidi@mail.mcgill.ca>
2018-05-03 13:09:12 -0400

    delta function added

--- exocartographer/delta_function.py
+++ exocartographer/delta_function.py
@@ -25,84 +25,27 @@ def analytic_delta(lat, long, w_rot, w_orb, obliq, i, sol_phase, orb_pos, sub_lo
     flux = []
 
     for t in time: 
-
         cos_phi_knot = np.cos(w_rot*t)
-        #print("Debugging: cos_phi_knot:")
-        #print(cos_phi_knot)
-
         cos_theta_knot = np.cos(i)*np.cos(obliq) + np.sin(i)*np.sin(obliq)*np.cos(sol_phase)
-        #print("Debugging: cos_theta_knot:")
-        #print(cos_theta_knot)
-
         sin_theta_knot = np.sqrt(1-(cos_theta_knot)**2)
-        #print("Debugging: sin_theta_knot")
-        #print(sin_theta_knot)
-
         sin_theta = np.sin(theta)
-        #print("Debugging: sin_theta")
-        #print(sin_theta)
-
         cos_theta = np.cos(theta)
-        #print("Debugging: cos_theta")
-        #print(cos_theta)
-
         cos_theta_s = np.sin(obliq)*np.cos(w_orb*t - sub_long)
-        #print("Debugging: cos_theta_s")
-        #print(cos_theta_s) 
-
         sin_phi_knot = -np.sin(w_rot*t)  
-        #print("Debugging: sin_phi_knot")
-        #print(sin_phi_knot) 
-
         sin_theta_s = np.sqrt(1-(cos_theta_s)**2)          
-        #print("Debugging: sin_theta_s")
-        #print(sin_theta_s)  
-
         cos_piw = np.sin(i)*np.cos(w_orb*t)
-        #print("Debugging: cos_piw")
-        #print(cos_piw) 
-
         cos_phi_knot_s = (cos_piw - (cos_theta_knot*cos_theta_s))/(sin_theta_knot * sin_theta_s)
-        #print("Debugging: cos_phi_knot_s")
-        #print(cos_phi_knot_s) 
-
         sin_phi_knot_s = np.sqrt(1-(cos_phi_knot_s)**2)                           
-        #print("Debugging: sin_phi_knot_s:")
-        #print(sin_phi_knot_s) 
-
         sin_phi_s = sin_phi_knot*cos_phi_knot_s + cos_phi_knot*sin_phi_knot_s
-        #print("Debugging sin_phi_s:")
-        #print(sin_phi_s)
-
         cos_phi_s = cos_phi_knot*cos_phi_knot_s - sin_phi_knot*sin_phi_knot_s
-        #print("Debugging cos_phi_s")
-        #print(cos_phi_s)
 
         #Visibility and illumination 
-
         cos_phi_phi_knot = np.cos(phi)*cos_phi_knot + np.sin(phi)*sin_phi_knot
-        #print("Debugging cos_phi_phi_knot")
-        #print(cos_phi_phi_knot)
-
         cos_phi_phi_s = np.cos(phi)*cos_phi_s + np.sin(phi)*sin_phi_s 
-        #print("Debugging cos_phi_phi_s")
-        #print(cos_phi_phi_s)
-
         Vnz = np.sin(theta)*sin_theta_knot*cos_phi_phi_knot + np.cos(theta)*cos_theta_knot 
-        
-        #print("Debugging Visbility nz: ")
-        #print(Vnz)
-
         Inz = np.sin(theta)*sin_theta_s * cos_phi_phi_s + np.cos(theta)*cos_theta_s 
-        #print("Debugging Illumination nz")
-        #print(Inz)
-
         single_flux = (1/np.pi)*(max(Vnz, 0))*(max(Inz, 0))
-        #print("Debugging single_flux")
-        #print(single_flux)
-
         flux.append(single_flux)
-    
     return flux 
 
 
@@ -118,8 +61,8 @@ i =1
 sol_phase = 1
 orb_pos = 1
 sub_long = 1
-theta = np.pi/2
-phi = np.pi/2
+theta = np.pi/4
+phi = np.pi/4
 
 print(analytic_delta(lat, long, w_rot, w_orb, obliq, i, sol_phase, orb_pos, sub_long, time, theta, phi))
 
