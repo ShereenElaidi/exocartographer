c29640493b35cfe98969b587e0f1a4fbfc540f8d
Shereen Elaidi <shereen.elaidi@mail.mcgill.ca>
2018-06-28 11:43:31 -0400

    implemented numerical solution & comparison

--- exocartographer/is_analytic_faster.py
+++ exocartographer/is_analytic_faster.py
@@ -3,8 +3,10 @@ from analytic_delta import analytic_delta
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
+from scipy.spatial import distance
 from math import sqrt
 from numerically_integrate import numerically_integrate
+import healpy as hp
 
 # SIMULATING AN ENTIRE PLANET'S LIGHTCURVE
 def make_lightcurve_plot(flux):
@@ -13,16 +15,14 @@ def make_lightcurve_plot(flux):
     plt.xlabel('time(rotations)')
     plt.show()
 
-# Array of Lat (theta) and Lng (phi)
+# This will simulate an entire planet
 theta = [0,np.pi/6, np.pi/5, np.pi/4, np.pi/3, np.pi/2,
-         2*np.pi/3, 3*np.pi/4, 5*np.pi/6, np.pi, 7*np.pi/6,
-         5*np.pi/4,4*np.pi/3, 5*np.pi/3, 7*np.pi/4, 11*np.pi/6]
+         2*np.pi/3, 3*np.pi/4, 5*np.pi/6, np.pi]
 phi = [0,np.pi/6, np.pi/5, np.pi/4, np.pi/3, np.pi/2,
-         2*np.pi/3, 3*np.pi/4, 5*np.pi/6, np.pi, 7*np.pi/6,
-         5*np.pi/4,4*np.pi/3, 5*np.pi/3, 7*np.pi/4, 11*np.pi/6]
+         2*np.pi/3, 3*np.pi/4, 5*np.pi/6, np.pi]
 
 
-time_data = np.array([0,1,2,3,4,5,6,7,8,9,10])
+time_data = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 w_rot = 1
 w_orb = 1
 obliq = 1
@@ -30,11 +30,14 @@ i = 1
 sol_phase = 1
 orb_pos = 1
 sub_long = 1
+colat = np.pi/3                    # location of bright point
+colng = np.pi/3                    # location of bright point
+size = 2                            # pixel size for numerical integration
+xi0 = 1
 
+# -----------------CALCULATING THE TIME DIFFERENCE---------------------------------
 
-# CALCULATING THE TIME DIFFERENCE
-
-# Put code for analytic in here
+# Put code for analytic in here:
 t0 = t.clock()
 for k in range(0, len(theta)):
     for j in range(0, len(phi)):
@@ -43,46 +46,56 @@ for k in range(0, len(theta)):
 t1 = t.clock()
 analytic_time = t1-t0
 
-# Put code for numerical integration here
+# Put code for numerical integration here:
+
+# def numerically_integrate(lat, lng, times, wrot, worb, obq, inc, xisol, xi0, colat, colng, size):
 t2 = t.clock()
 for k in range(0, len(theta)):
     for j in range(0, len(phi)):
-        flux = analytic_delta(w_rot, w_orb, obliq, i, sol_phase, orb_pos,
-                              sub_long, time_data, int(theta[k]), int(phi[j]))
+        flux = numerically_integrate(theta[k], phi[j], time_data, w_rot, w_orb, obliq, i, sol_phase, xi0,  colat, colng, size)
+
 t3 = t.clock()
 numerical_time = t3 - t2
 
-print("Analytic time: %f" %(analytic_time))
-print("Numeric integration time: %f" % (numerical_time))
+print("Computing values....")
 
-# CALCULATING THE ACCURACY OF THE ANALYTIC MAP
+# -------CALCULATING THE ACCURACY OF THE ANALYTIC MAP-----------------
 
 # True values - the values of the flux for the numerical integration:
 true_flux = []
 
-# Filler code for now until talk to Nick
-
-
-
 for k in range(0, len(theta)):
     for j in range(0, len(phi)):
-        true_flux.append(numerically_integrate(theta[i], phi[j], time_data, w_rot, w_orb, obliq, i, sol_phase, orb_pos))
+        true_flux.append(numerically_integrate(theta[k], phi[j], time_data, w_rot, w_orb, obliq, i, sol_phase, orb_pos,
+                                               colat, colng, size))
 
 # Estimated values - the estimated flux values w/ analytic
 analytic_flux = []
 for k in range(0, len(theta)):
     for j in range(0, len(phi)):
         analytic_flux.append(analytic_delta(w_rot, w_orb, obliq, i, sol_phase, orb_pos,
-                              sub_long, time_data, int(theta[k]), int(phi[j])))
+                              sub_long, time_data, theta[k], phi[j]))
 
 
 def calculate_rmse(true_flux, analytic_flux):
     return sqrt(mean_squared_error(true_flux, analytic_flux))
 
 
+def euclidean_distance(true_flux, analytic_flux, times):
+    euclidean_distance = 0
+    for row in range(0, len(true_flux)):
+        euclidean_distance+= distance.euclidean(true_flux[row], analytic_flux[row])
+    return euclidean_distance, euclidean_distance/(len(times)*len(true_flux))
+
+
 print("-------------------SUMMARY------------------------")
-print('Statistics for time: ')
-print("Analytic time: %f" %(analytic_time))
-print("Numeric integration time: %f" % (numerical_time))
-print("Accuracy statistics:")
+print('TIME STATISTICS:')
+print("Analytic time: %f (seconds)" %(analytic_time))
+print("Numeric integration time: %f (seconds)" % (numerical_time))
+print("ACCURACY STATISTICS")
 print("RMSE: %f" %(calculate_rmse(true_flux, analytic_flux)))
+euclideandistance, average_euclidean_distance = euclidean_distance(true_flux, analytic_flux, time_data)
+print("Euclidean distance: %f \nAverage Euclidean Distance: %f" % (euclideandistance, average_euclidean_distance))
+print("Making plots...")
+make_lightcurve_plot(true_flux)
+make_lightcurve_plot(analytic_flux)
--- exocartographer/numerically_integrate.py
+++ exocartographer/numerically_integrate.py
@@ -3,14 +3,31 @@ from analytic_kernel import kernel
 import numpy as np
 from scipy.integrate import dblquad
 
-# Basic integration: double integral, Kernel*map*sintheta
-
-
 # TODO: ask about this:
-def map(lat, lng):
-    return lat*lng
+def map(lat, lng, colat, colng, size):
+    '''A function to mimic the delta function for the numerical solution. If the given longitude and latitude
+    are within the bright pixel (defined by the size parameter), return 1. Else, return 0.
+
+    Inputs:
+    :param lat: the latitude of the point
+    :param lng: the longitude of the point
+    :param colat: the latitude of the bright point
+    :param colng: the longitude of the bright point
+    :param size: the size of the pixel.
+
+    Output: 0 or 1 based on if the lat and lng fall within the pixel containing colat and colng'''
+
+    if (lat <= size*colat or lat >= -1*size*colat) and (lng <= size*colng or lat >= -1*size*colng):
+        return 1
+    else:
+        return 0
 
 def get_kernel(times, wrot, worb, obq, inc, xisol, xi0, lat, lng):
+    '''A function to obtain the kernel for the numerical integration solution.
+
+    Inputs: an array of time-values (discrete), wrot, worb, obq, inc, xisol, xi0, lat, and lng
+
+    Output: the value of the kernel'''
     view_geom = viewgeom(times, wrot, worb, obq, inc, xisol, xi0)
     sth = np.sin(lat)
     cth = np.cos(lat)
@@ -18,9 +35,11 @@ def get_kernel(times, wrot, worb, obq, inc, xisol, xi0, lat, lng):
     cph = np.cos(lng)
     return kernel(sth, cth, sph, cph, view_geom)
 
-def numerically_integrate(lat, lng, times, wrot, worb, obq, inc, xisol, xi0):
+def numerically_integrate(lat, lng, times, wrot, worb, obq, inc, xisol, xi0, colat, colng, size):
+    '''A function to implement the numerical solution: numerically integrating the varying flux from a small
+    pixel. '''
     flux = []
-    integrand = get_kernel(times, wrot, worb, obq, inc, xisol, xi0, lat, lng)
+    integrand = get_kernel(times, wrot, worb, obq, inc, xisol, xi0, lat, lng)*map(lat, lng, colat, colng, size)*np.sin(lat)
     for t in range(0, len(integrand)):
         flux.append(dblquad(lambda lat, lng: integrand[t], 0, np.pi, lambda theta:0,
                        lambda theta: 2*np.pi)[0])
